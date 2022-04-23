b82571b2a16a9cf87aadce614b5737ce71587985
Bryan Duggan <bryan.duggan@tudublin.ie>
2022-04-23 20:30:19 +0100

    nema1

--- GE2 Test 2022/Assets/Nematode.cs
+++ GE2 Test 2022/Assets/Nematode.cs
@@ -17,7 +17,34 @@ public class Nematode : MonoBehaviour
 
     void Awake()
     {
-        
+        for(int i = 0 ; i < length ; i ++)
+        {
+            GameObject seg = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+            Vector3 pos = new Vector3(0, 0, i);
+            pos = transform.TransformPoint(pos);
+            seg.transform.position = pos;
+            seg.transform.rotation = this.transform.rotation;
+            seg.GetComponent<Renderer>().material = material;
+        }
+        float s = 0.1f;
+        switch (gender)
+        {
+            case Gender.Male:
+                GameObject penis = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+                penis.transform.Rotate(90, 0, 0);
+                penis.transform.localScale = new Vector3(0.1f, 1, 0.1f);
+                Vector3 pos = new Vector3(0,0,length + 1);
+                pos = transform.TransformPoint(pos);
+                penis.transform.position = pos;
+                break;
+            case Gender.Female:
+                GameObject vagina = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+                vagina.transform.localScale = new Vector3(s, s, s);
+                Vector3 p = new Vector3(0, 0.5f + (s * 0.5f), length);
+                vagina.transform.position = p;
+                break;
+
+        }
     }
 
 
