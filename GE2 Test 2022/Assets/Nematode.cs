511028892acc3560aa2853f9369266c93f19181c
Bryan Duggan <bryan.duggan@tudublin.ie>
2022-04-24 01:16:36 +0100

    bla

--- GE2 Test 2022/Assets/Nematode.cs
+++ GE2 Test 2022/Assets/Nematode.cs
@@ -6,6 +6,8 @@ public class Nematode : MonoBehaviour
 {
     public int length = 5;
 
+    public Material material;
+
     void Awake()
     {
         GameObject head = null;
@@ -21,8 +23,11 @@ public class Nematode : MonoBehaviour
             Vector3 pos = new Vector3(0, 0, -i);
             pos = transform.TransformPoint(pos);
             seg.transform.position = pos;
+            float s = 1 + (i * 3 / (float) length);
+            seg.transform.localScale = new Vector3(s, 1, 1);
             seg.transform.rotation = this.transform.rotation;
             seg.transform.parent = this.transform;
+            seg.GetComponent<Renderer>().material = material;
             seg.GetComponent<Renderer>().material.color = Color.HSVToRGB(i / (float) length, 1.0f, 1.0f); 
         }
 
@@ -31,7 +36,7 @@ public class Nematode : MonoBehaviour
         c.weight = 3;
         c.radius = 20;
 
-        head.AddComponent<NoiseWander>().weight = 3;;
+        head.AddComponent<NoiseWander>().weight = 6;;
     }
 
 
